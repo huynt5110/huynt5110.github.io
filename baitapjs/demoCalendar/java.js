582fa7e8f39bea450e73aa8ff07e8e4d07061ec5
huy.nt5110 <huy.nt5110@gmail.com>
2017-07-28 14:23:13 +0700

    update calendar js

--- baitapjs/demoCalendar/java.js
+++ baitapjs/demoCalendar/java.js
@@ -1,41 +1,48 @@
-var d = new Date();
-var month = d.getMonth();  // 6 because i start form 0
-var year = d.getFullYear(); //2017
+var date = new Date();
+var month = date.getMonth();  // 6 because i start form 0
+var year = date.getFullYear(); //2017
 var alldayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-var count = 1;
 var div = document.getElementById("div_contain");
-var currentday = d.getDate();
-var curentmonth = d.getMonth();
-var currentyear = d.getFullYear();
+var table = document.getElementsByClassName("table");
+var textbox = document.getElementById("text_box");
+var currentday = date.getDate();// get current day
+var curentmonth = date.getMonth();
+var currentyear = date.getFullYear();
 var flag = false;
+
 function callfunction() {
+	div.style.display = "block"; // when user click on input table will appear
 	var firstDay = new Date(year, month, 1).toDateString();   // 1 7 2017
-	var lastDay = new Date(year, month+1 , 0); //31 7 2017  put 0 in day, can't put 30 or 29 
+	var lastDay = new Date(year,month+1 , 0); //31 7 2017  put 0 in day, can't put 30 or 29 
 	var days = new Date(year, month+1, 0).getDate(); //31   get max day of year and month 
 	var day_name =  firstDay.substring(0, 3); //sat  name of day
 	var index_day = alldayofweek.indexOf(day_name); //6  positon number
 	if(flag == false)
+	{
 		get_calendar(days ,index_day);
+	}
 }
 function get_calendar(days ,index_day) {
 	var table = document.createElement('table');
 	table.setAttribute('class','table');
-	table.border = 2;
+	table.border = 1;
 	var tr = document.createElement('tr');
 	var td = document.createElement('td');
 	// create previous year <<
 	var td_previousY = document.createElement('button');
 	td_previousY.innerHTML = "<<";
 	td_previousY.onclick = function() {
-            if (year > 1950) {
-                year--;
-				//remove all child of div 
-				while (div.firstChild) {
-					div.removeChild(div.firstChild);
-					}
-					flag = false;
+        if (year > 1950) {
+            year--;
+			//remove all child of div 
+			while (div.firstChild) {
+				div.removeChild(div.firstChild);
+				}
+				flag = false;
                 callfunction();
-            }
+        } else {
+			alert("Too far from current year");
+		}
         };		
 	td.appendChild(td_previousY);
 	tr.appendChild(td);
@@ -44,54 +51,59 @@ function get_calendar(days ,index_day) {
 	var td_previousM = document.createElement('button');
 	td_previousM.innerHTML = "<";
 	td_previousM.onclick = function() {
-            if (month > 0) {
-                month--;
-				while (div.firstChild) {
-					div.removeChild(div.firstChild);
-					}
-					flag = false;
-                callfunction();
-            }
-        };
+        if (month >= 0) {
+            month--;
+			//month start from 0 so -1 mean month 12 of pervious year
+			if(month == -1){
+				month = 11;
+				year-=1;
+			}
+		while (div.firstChild) {
+			div.removeChild(div.firstChild);
+		}
+		flag = false;
+        callfunction();
+        }
+    };
 	td.appendChild(td_previousM);
 	tr.appendChild(td);
 	//dropdown month anh year
 	var td = document.createElement('td');
 	var dropdown_month = document.createElement("select");
-	for(i = 1;i<13;i++)
+	for(index_month = 1;index_month<13;index_month++)
 	{
 		var option = document.createElement("option");
-		option.value = i;
-		if (i-1 == month) {
+		option.value = index_month;
+		if (index_month-1 == month) {
             option.selected = true;
         }
-		var textnodeM = document.createTextNode("Tháng "+i);
+		var textnodeM = document.createTextNode("Tháng "+index_month);
 		option.appendChild(textnodeM);
 		dropdown_month.appendChild(option);
 	}
 	td.appendChild(dropdown_month);
 	dropdown_month.onchange = function() {
-		month = parseInt(this.options[this.selectedIndex].value);
+		month = parseInt(this.options[this.selectedIndex].value -1);
 		while (div.firstChild) {
 			div.removeChild(div.firstChild);
 		}
 			flag = false;
             callfunction();
 	}
-	
 	var dropdown_year = document.createElement("select");
-	for (var j = 1950; j <= (d.getFullYear() + 50); j++) {
+	for (var index_year = 1950; index_year <= (date.getFullYear() + 50); index_year++) {
         var option = document.createElement('option');
-		option.value = j;
-		var textnodeM = document.createTextNode(j);
+		option.value = index_year;
+		var textnodeM = document.createTextNode(index_year);
 		option.appendChild(textnodeM);
-        if (j == year) {
+        if (index_year == year) {
             option.selected = true;
         }
         dropdown_year.appendChild(option);
 	}
 	td.appendChild(dropdown_year);
 	td.colSpan = 3;
+	// year dropdown change
 	dropdown_year.onchange = function() {
 		year = parseInt(this.options[this.selectedIndex].value);
 		while (div.firstChild) {
@@ -106,19 +118,19 @@ function get_calendar(days ,index_day) {
 	var td_nextM = document.createElement('button');
 	td_nextM.innerHTML = ">";
 	td_nextM.onclick = function() {
-            if (month > 0) {
-                month++;
-				if(month >12){
-					month = 1;
-					year+=1;
-				}
-				while (div.firstChild) {
-					div.removeChild(div.firstChild);
-					}
-					flag = false;
-                callfunction();
-            }
-        };	
+        if (month >= 0) {
+            month++;
+			if(month == 12){
+				month = 0;
+				year+=1;
+			}
+		while (div.firstChild) {
+			div.removeChild(div.firstChild);
+		}
+		flag = false;
+        callfunction();
+        }
+    };	
 	td.appendChild(td_nextM);
 	tr.appendChild(td);
 	// create previous year >>
@@ -126,15 +138,18 @@ function get_calendar(days ,index_day) {
 	var td_nextY = document.createElement('button');
 	td_nextY.innerHTML = ">>";
 	td_nextY.onclick = function() {
-            if (year < 2067) {
-                year++;
-				while (div.firstChild) {
-					div.removeChild(div.firstChild);
-					}
-					flag = false;
-                callfunction();
-            }
-        };		
+        if (year < 2067) {
+        year++;
+		while (div.firstChild) {
+			div.removeChild(div.firstChild);
+		}
+		flag = false;
+        callfunction();
+        }
+		else {
+			alert("Too far from current year");
+		}
+    };		
 	td.appendChild(td_nextY);
     tr.appendChild(td);
 	table.appendChild(tr);
@@ -146,11 +161,11 @@ function get_calendar(days ,index_day) {
         tr.appendChild(td);
     }
     table.appendChild(tr);
-	//Print day empty in month
+	//If  < day_index will be "" in month
     tr = document.createElement('tr');
-	var c;
-    for (c = 0; c <= 6; c++) {
-        if (c == index_day) {
+	var checkday= 0;
+    for (checkday ; checkday <= 6; checkday++) {
+        if (checkday == index_day /*first day of month*/) {
             break;
         }
         td = document.createElement('td');
@@ -159,35 +174,45 @@ function get_calendar(days ,index_day) {
     }
 	table.appendChild(tr);
 	//day in month
-	for (; c <= 6; c++) {
+	daynumber = 1;
+	for (checkday; checkday <= 6; checkday++) {
             td = document.createElement('td');
-            td.innerHTML = count;
-            count++;
+            td.innerHTML = daynumber;
+			td.onclick = choseDate;
+            daynumber++;
             tr.appendChild(td);
-        }
+    }
     table.appendChild(tr);
-	for (var r = 0; r <= 4; r++) {
-            tr = document.createElement('tr');
-            for ( c = 0; c <= 6; c++) {
-				if(count>days)
-				{
-					table.appendChild(tr);
-					break;
-				}
-				if (count-1 == currentday && month == curentmonth && year == currentyear) {
-                td.style.cssText = "background-color: blue";
-            }
-				td = document.createElement('td');
-                td.innerHTML = count;
-                count++;
-                tr.appendChild(td);
-            }
-            table.appendChild(tr);
+	for (var row = 0; row <= 4; row++) {
+        tr = document.createElement('tr');
+        for ( colum = 0; colum <= 6; colum++) {
+			if(daynumber>days)
+			{
+				table.appendChild(tr);
+				break;
+			}
+			//when currentday,current,current year mark with blue
+			if (daynumber-1 == currentday && month == curentmonth && year == currentyear) {
+			td.style.cssText = "background-color: blue";
+			}
+			td = document.createElement('td');
+			td.innerHTML = daynumber;
+			td.onclick = choseDate;
+			daynumber++;
+			tr.appendChild(td);
         }
-	count = 1;
+        table.appendChild(tr);
+    }
 	// create table
 	div.appendChild(table);
-	flag = true;
+	flag = true;// if flag true div will not create table
 }
+function choseDate() {
+		var userchoose;  // get day user choose
+		userchoose = this.innerHTML + "/" +(parseInt(month)+1) + "/" + year ;
+		textbox.value = userchoose;
+		div.style.display = "none"; 
+    }
+
 	
 
